327b9d74c88dac196ee77477283b55c16cbec686
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 05:38:46 +0500

    Task1

---
+++ Tyuiu.SavchenkoEI.Sprint2.Task1.V25.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SavchenkoEI.Sprint2.Task1.V25.Lib
+{
+    public class DataService : ISprint2Task1V25
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = ((a < d) | (b > d));
+            res[1] = ((a < b) & (c > b));
+            res[2] = ((a < c) || (d > c));
+            res[3] = ((a > d) && (d < a));
+            res[4] = !(!res[0]);
+            res[5] = ((a > d) ^ (d < b));
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint2.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint2.Task1.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
