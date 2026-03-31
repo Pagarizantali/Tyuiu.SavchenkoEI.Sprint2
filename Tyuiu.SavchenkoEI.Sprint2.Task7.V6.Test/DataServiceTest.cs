6954c203c85ebff846c1a5191d3f2d123d3dd996
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 06:28:06 +0500

    Task7

---
+++ Tyuiu.SavchenkoEI.Sprint2.Task7.V6.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint2.Task7.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
